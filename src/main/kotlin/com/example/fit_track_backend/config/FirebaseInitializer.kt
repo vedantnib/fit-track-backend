f0b2230175b3e9bdd0915b438310b4999f6d7063
Vedant Nibandhe <vnibandhe@gmail.com>
2024-07-14 20:07:50 -0500

    Feature: Modified firebase initializer to read from env variables

--- src/main/kotlin/com/example/fit_track_backend/config/FirebaseInitializer.kt
+++ src/main/kotlin/com/example/fit_track_backend/config/FirebaseInitializer.kt
@@ -1,26 +1,46 @@
 package com.example.fit_track_backend.config
+
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.stereotype.Service
-import java.io.FileInputStream
-import java.io.IOException
+import java.io.ByteArrayInputStream
 import javax.annotation.PostConstruct
 
 @Service
-public class FirebaseInitializer {
+class FirebaseInitializer {
 	@PostConstruct
-    public fun initializeFirebase() {
+	fun initializeFirebase() {
 		try {
-			val serviceAccount = FileInputStream("src/main/resources/beta-dbved-firebase-adminsdk-o0sn7-c41268a78e.json")
-			val options: FirebaseOptions = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build()
-			FirebaseApp.initializeApp(options)
-		}
-		catch (exception: Exception) {
-			print("Error "+ exception.message)
-		}
+			val privateKey = System.getenv("GOOGLE_PRIVATE_KEY")?.replace("\\n", "\n")
+				?: throw IllegalStateException("Environment variable GOOGLE_PRIVATE_KEY is not set")
 
-    }
+			val options = FirebaseOptions.builder()
+				.setCredentials(
+					GoogleCredentials.fromStream(
+						ByteArrayInputStream(
+							("{\"type\": \"service_account\"," +
+									"\"project_id\": \"" + System.getenv("GOOGLE_PROJECT_ID") + "\"," +
+									"\"private_key_id\": \"" + System.getenv("GOOGLE_PRIVATE_KEY_ID") + "\"," +
+									"\"private_key\": \"" + privateKey + "\"," +
+									"\"client_email\": \"" + System.getenv("GOOGLE_CLIENT_EMAIL") + "\"," +
+									"\"client_id\": \"" + System.getenv("GOOGLE_CLIENT_ID") + "\"," +
+									"\"auth_uri\": \"" + System.getenv("GOOGLE_AUTH_URI") + "\"," +
+									"\"token_uri\": \"" + System.getenv("GOOGLE_TOKEN_URI") + "\"," +
+									"\"auth_provider_x509_cert_url\": \"" + System.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL") + "\"," +
+									"\"client_x509_cert_url\": \"" + System.getenv("GOOGLE_CLIENT_X509_CERT_URL") + "\"," +
+									"\"universe_domain\": \"" + System.getenv("GOOGLE_UNIVERSE_DOMAIN") + "\"}")
+								.toByteArray()
+						)
+					)
+				)
+				.build()
+
+			FirebaseApp.initializeApp(options)
+			println("Firebase initialized successfully")
+		} catch (exception: Exception) {
+			println("Error initializing Firebase: ${exception.message}")
+			exception.printStackTrace()
+		}
+	}
 }
